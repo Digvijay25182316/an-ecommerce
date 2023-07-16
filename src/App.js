a5b287010061a6134589f57ca0209c8f718b873b
Digvijay Edake <digvijays.edake>
2023-07-16 08:25:10 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ import Loading from './components/MainComponents/Loading';
 
 export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 // 'https://an-ecommerce-backend.onrender.com/api/v1';
-('http://localhost:4000/api/v1');
+// ('http://localhost:4000/api/v1');
 
 function App() {
   return (
