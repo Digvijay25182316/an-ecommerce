a52687f9579b341cf3476a7d4f2de79506d8fb3f
Digvijay Edake <digvijays.edake>
2023-07-17 10:43:51 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
+import { ContextProvider } from './context/store';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript />
-      <App />
-    </ChakraProvider>
+    <ContextProvider>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript />
+        <App />
+      </ChakraProvider>
+    </ContextProvider>
   </StrictMode>
 );
